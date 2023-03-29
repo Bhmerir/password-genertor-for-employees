dcbd7301e30167d09575b759ed34f07329948223
Bahareh <mer_ir@yahoo.com>
2023-03-28 22:35:15 -0700

    Add function of 'askCharacterNumber' to prompt user about the requested number of characters

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//The global variable of characterNo ia available in all of functions
+var characterNo;
+
+//This function ask the user to enter the number of characters requested 
+function askCharactorNumber(){
+  var notAccepted = true;
+  // This why will be executed until the user enters a valid number or click on cancel button
+  while(notAccepted){
+    characterNo = prompt("How many characters would you like your password to contain?");
+    if (characterNo != null){
+      characterNo = parseInt(characterNo);
+      // Number.isInteger checks if the entered value is an integer or not. AS the user might enter for example an string or leave it empty which are unacceptable
+      if (!Number.isInteger(characterNo)){
+        alert ("You have to enter an integer number.");
+      }
+      else if (characterNo < 8){
+        alert("Password length must be at least 8 characters.");
+      }
+      else if (characterNo > 128){
+        alert("Password length must be less than 129 characters.");
+      }
+      else{
+        //If the user enter an integer between 8 and 129, loop will be ended
+        notAccepted = false;
+      }
+    }
+    else{
+       //If the user click cancel, loop will be ended
+      notAccepted = false;
+    }
+  }
+}
+
+function generatePassword(){
+   //This function generates the password
+   askCharactorNumber();
+}
+
